c793c9bd5fd8a50d14c35b141258e22e4a91b6ca
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-03-20 22:35:29 +0530

    Initial Commit

---
+++ 01 require/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var request = 0;
+
+var handleRequest = function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Welcome to Node Essential Training\n' + ++request);
+};
+
+var server = http.createServer(handleRequest);
+
+server.listen(3000, 'localhost');
---
+++ 03 modules/flight/index.js
@@ -0,0 +1,13 @@
+var number, origin, destination
+
+exports.setNumber = function (num) {
+	number = num;
+}
+
+exports.setOrigin = function (org) {
+	origin = org;
+}
+
+exports.setDestination = function (des) {
+	destination = des;
+}
